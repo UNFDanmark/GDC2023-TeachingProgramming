b823c3cad18d2524b9f5de418b87daf3a0d989a5
Maxime Birkkjær Havez <maxih@hotmail.dk>
2023-07-14 16:52:45 +0200

    Implemented shooting

--- TeachingProgrammingUnityProject/Assets/Scripts/PlayerScript.cs
+++ TeachingProgrammingUnityProject/Assets/Scripts/PlayerScript.cs
@@ -9,13 +9,21 @@ public class PlayerScript : MonoBehaviour
     [SerializeField]
     private Rigidbody body;
 
+    [SerializeField] private GameObject bullet;
+
+    [SerializeField] private Transform gunBarrel;
+
     [SerializeField] private float speed;
     [SerializeField] private float turnSpeed = 1;
     [SerializeField] private float jumpSpeed = 1;
+    [SerializeField] private float bulletLifeTime = 9;
+    [SerializeField] private float bulletSpeed = 9;
+    [SerializeField] private float cooldownTime = .9f;
 
     private float moveInput;
     private float turnInput;
     private bool isOnGround;
+    private float timeLeftBetweenShots;
     
     // Start is called before the first frame update
     void Start()
@@ -28,10 +36,21 @@ public class PlayerScript : MonoBehaviour
     {
         moveInput = Input.GetAxis("Vertical");
         turnInput = Input.GetAxis("Horizontal");
+
+        timeLeftBetweenShots -= Time.deltaTime;
+        
         if (Input.GetButtonDown("Jump") && isOnGround)
         {
             body.velocity = Vector3.up * jumpSpeed;
             isOnGround = false;
+        }
+
+        if (Input.GetButtonDown("Fire1") && timeLeftBetweenShots <= 0)
+        {
+            GameObject newBullet = Instantiate(bullet,gunBarrel.position,transform.rotation);
+            newBullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+            Destroy(newBullet,bulletLifeTime);
+            timeLeftBetweenShots = cooldownTime;
         }
     }
 
